b5bead9ef5363b17f54fc214da4baf172267d4ac
udacodingid <mentoring@udacoding.com>
2021-10-28 17:42:08 +0700

    toaster

--- mytoast/src/main/java/com/udacoding/mytoast/ToastMessage.kt
+++ mytoast/src/main/java/com/udacoding/mytoast/ToastMessage.kt
@@ -6,7 +6,7 @@ import android.widget.Toast
 
 
 
-class ToastMessage {
+class ToasterMessage {
 
     fun s(c: Context?, message: String?) {
         Toast.makeText(c, message, Toast.LENGTH_SHORT).show()
